a952df05e8e4ec73ed90c4a1105b522d590065e4
ilhamsitok <nooneknow684@gmail.com>
2018-05-12 21:01:10 +0700

    Test 2

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+  include 'koneksi.php';
+  $query = mysqli_query($conn,"select * from vwBuku");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  
+  <table>
+    <tr>
+      <td>No</td>
+      <td>ISBN</td>
+      <td>Judul</td>
+      <td>Pengarang</td>
+      <td>Penerbit</td>
+      <td>Tahun Terbit</td>
+      <td>Tersedia</td>
+    </tr>
+    <?php 
+      $no=1;
+      while($data = mysqli_fetch_array($query)){
+    ?>
+    <tr>
+      <td><?=$no++?></td>
+      <td><a href="#"><?=$data['judul']?></a></td>
+      <td><?=$data['pengarang']?></td>
+      <td><?=$data['nama_instansi']?></td>
+      <td><?=$data['tahun_terbit']?></td>
+      <td><?=($data['jumlah']>0)?'tersedia':'kosong'?></td>
+    </tr>
+    <?php } ?>
+  </table>
+
+</body>
+</html>
