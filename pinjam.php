6db542f07a6dbe51d2df10d2c4b49b4ce48a38da
ilhamsitok <nooneknow684@gmail.com>
2018-05-14 10:59:25 +0700

    pinjam php

--- pinjam.php
+++ pinjam.php
@@ -1,18 +1,31 @@
 <?php
-    include "koneksi.php"
+    include "koneksi.php";
     session_start();
     $isbn = $_GET["isbn"];
     $username = $_SESSION["username"];
-    if ($ispinjam){
+    $isPinjam = $_SESSION["isPinjam"];
+    if (!$isPinjam){
         $sql = "select * from tblStatusPeminjaman where id_user='$username' and tanggal_peminjaman is NULL";
         $result = mysqli_query($conn,$sql);
-        $row = mysqli_fetch_row($result);
-        if ($row){
-            
-        }else{
-
+        $row = mysqli_num_rows($result);
+        if (!$row){
+            $sql = "insert into tblStatusPeminjaman(id_user) values ('$username')";
+            $result = mysqli_query($conn,$sql);
+            $sql = "select * from tblStatusPeminjaman where id_user='$username' and tanggal_peminjaman is NULL";
+            $result = mysqli_query($conn,$sql);
+            $data = mysqli_fetch_row($result);
+            $idpinjam = $data[0];
         }
-        
+        $data = mysqli_fetch_row($result);
+        $idpinjam = $data[0];
+
+        $sql = "insert into tblListBuku values ($idpinjam,'$isbn')";
+        $result = mysqli_query($conn,$sql);
+        if (!$result){
+            echo "anda sudah memesan buku ini";
+        }else{
+            header("location:/bookstore/BookStore");
+        }
     }
 
 ?>
